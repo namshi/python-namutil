4ed6c266292305b0db32650eed059362aaa38c82
hzarka <hzarka@gmail.com>
2016-01-04 12:31:38 +0400

    Update hz.py

--- namutil/hz.py
+++ namutil/hz.py
@@ -754,3 +754,22 @@ def insert_into_batch(engine, table, rows, batch=500, engines={}, tables={}):
         engine.execute(query, group)
     return engine
 
+def get_days_ago(days=1):
+    import datetime
+    return (datetime.datetime.now() - datetime.timedelta(days)).strftime("%Y%m%d")
+
+def date_range(start_date=None, end_date=None):
+    import datetime
+    
+    start_date = start_date or get_days_ago()
+    if '-' in start_date:
+        start_date, end_date = start_date.split('-', 1)
+    end_date = end_date or start_date
+
+    start_date = datetime.datetime.strptime(start_date, "%Y%m%d")
+    end_date = datetime.datetime.strptime(end_date, "%Y%m%d")
+
+    for n in range(int((end_date - start_date).days) + 1):
+        yield (start_date + datetime.timedelta(days=n)).strftime("%Y%m%d")
+
+
