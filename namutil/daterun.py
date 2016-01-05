5c145b6bd63a76c6ae63d137be5566a3912e3a3d
Hisham Zarka <hzarka@gmail.com>
2016-01-05 16:10:53 +0400

    .

--- namutil/daterun.py
+++ namutil/daterun.py
@@ -6,6 +6,6 @@ import subprocess
 dates = sys.argv[1]
 
 for date in namutil.date_range(dates):
-    args = [v.replace("$DATE", date) for v in sys.argv[2:]]
+    args = [v.replace("$HDATE", "{}-{}-{}".format(date[:4], date[4:6], date[6:])).replace("$DATE", date) for v in sys.argv[2:]]
     subprocess.check_call(args, stdout=sys.stdout, stderr=sys.stderr)
 
